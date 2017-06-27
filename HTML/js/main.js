b1a61a0938f05358da75b39fea04a1d607c365fe
bestsuperweb <coolplum@coolplums-iMac.local>
2017-06-27 10:10:45 -0400

    ...

--- HTML/js/main.js
+++ HTML/js/main.js
@@ -164,30 +164,8 @@ jQuery(function($) {
 		});
 	});
 
-	//Google Map
-	var latitude = $('#google-map').data('latitude')
-	var longitude = $('#google-map').data('longitude')
-	function initialize_map() {
-		var myLatlng = new google.maps.LatLng(latitude,longitude);
-		var mapOptions = {
-			zoom: 14,
-			scrollwheel: false,
-			center: myLatlng
-		};
-		var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
-		var contentString = '';
-		var infowindow = new google.maps.InfoWindow({
-			content: '<div class="map-content"><ul class="address">' + $('.address').html() + '</ul></div>'
-		});
-		var marker = new google.maps.Marker({
-			position: myLatlng,
-			map: map
-		});
-		google.maps.event.addListener(marker, 'click', function() {
-			infowindow.open(map,marker);
-		});
-	}
-	google.maps.event.addDomListener(window, 'load', initialize_map);
+	
+	// google.maps.event.addDomListener(window, 'load', initialize_map);
 	
 });
 
